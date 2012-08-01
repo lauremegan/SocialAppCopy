0cd762c62426b206dd03e6730413eb887ce820a3
lauremegan <lauremegan@hotmail.com>
2012-08-01 19:28:03 +0100

    finish users paginate gravatar

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
 before_filter :signed_in_user, only: [:edit, :update]
-  before_filter :correct_user,   only: [:edit, :update]
+before_filter :correct_user,   only: [:edit, :update]
+before_filter :admin_user,     only: :destroy
 
 
 def index
